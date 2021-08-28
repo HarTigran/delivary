769add453e5959cedf560170f148f44af9e98610
Tigran Harutyunyan <harutyunyan.tigran.v@gmail.com>
2021-08-28 02:34:44 -0400

    Update main.py
    Check CD

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! Continuous delivery check'
+    return 'Hello World! Continuous delivery new check'
 
 @app.route('/echo/<name>')
 def echo(name):
